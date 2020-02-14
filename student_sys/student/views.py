6a97fd9472eaec75aa6e9be10d18b0e77fa8ab66
tangchuan <317403442@qq.com>
2020-02-14 10:19:49 +0800

    初始化提交

---
+++ student_sys/student/views.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.views import View
+
+from .forms import StudentForm
+from .models import Student
+
+# Create your views here.
+
+class IndexView(View):
+    template_name = 'index.html'
+
+    def get_context(self):
+        students =  Student.get_all()
+        context = {
+            'students':students
+        }
+        return context
+
+    def get(self, request):
+        context = self.get_context()
+        form = StudentForm()
+        context.update({
+            'form':form
+        })
+        return render(request,self.template_name,context=context)
+
+    def post(self,request):
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            student = Student()
+            student.name = cleaned_data['name']
+            student.sex = cleaned_data['sex']
+            student.email = cleaned_data['email']
+            student.profession = cleaned_data['profession']
+            student.qq = cleaned_data['qq']
+            student.phone = cleaned_data['phone']
+            form.save()
+            return HttpResponseRedirect(reverse('index'))
+        context = self.get_context()
+        context.update({
+            'form':form
+        })
+        return render(request,self.template_name,context=context)
